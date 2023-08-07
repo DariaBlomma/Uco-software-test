67f4d4b605723a9090ca3b69cda6335b010e849a
Дарья Козина <kozina.flowers@yandex.ru>
2023-08-07 11:19:44 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <ClientCard/>
+</template>
+
+<script setup lang="ts">
+import ClientCard from "./components/ClientCard.vue";
+import { clients, assignees } from "./test-data";
+</script>
+
+<style scoped lang="scss">
+
+</style>
